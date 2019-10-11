f5b276d17f783177e4d34554e656dac5f2295629
Luigi Vanacore <luigi.vanacore@outlook.com>
2019-10-11 05:08:05 +0200

    simple http server created

--- Http_Server/http-server.go
+++ Http_Server/http-server.go
@@ -1,1 +1,29 @@
-package Http_Server
+package main
+
+
+import
+(
+	"fmt"
+	"log"
+	"net/http"
+)
+
+const
+(
+	CONN_HOST = "localhost"
+	CONN_PORT = "8080"
+)
+
+
+func helloWorld( w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf( w, "Hello World!")
+}
+
+func main() {
+	http.HandleFunc("/", helloWorld)
+	err := http.ListenAndServe( CONN_HOST + ":" + CONN_PORT, nil)
+	if err != nil {
+	log.Fatal("error starting http server : ", err )
+	return
+	}
+}

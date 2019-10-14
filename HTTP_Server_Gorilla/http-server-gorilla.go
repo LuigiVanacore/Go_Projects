c7400dfd799ee14d3e95b0d04f93f3b607933f56
Luigi Vanacore <luigi.vanacore@outlook.com>
2019-10-14 16:46:38 +0200

    http_server_gorilla: added more handlers

--- HTTP_Server_Gorilla/http-server-gorilla.go
+++ HTTP_Server_Gorilla/http-server-gorilla.go
@@ -1,1 +1,46 @@
-package HTTP_Server_Gorilla
+package main
+
+import (
+	"io"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+const (
+	CONN_PORT = ":8080"
+)
+
+var GetRequestHandler = http.HandlerFunc(
+	func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Hello World"))
+	})
+
+var PostRequestHandler = http.HandlerFunc(
+	func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("It's a Post Request"))
+	})
+
+var PathVariableHandler = http.HandlerFunc(
+	func(w http.ResponseWriter, r *http.Request) {
+		vars := mux.Vars(r)
+		name := vars["name"]
+		w.Write([]byte("Hi " + name))
+	})
+
+func helloWorld(w http.ResponseWriter, r *http.Request) {
+	io.WriteString(w, "Hello World!")
+}
+
+func main() {
+	router := mux.NewRouter()
+	router.Handle("/", GetRequestHandler).Methods("GET")
+	router.Handle("/post", PostRequestHandler).Methods("POST")
+	router.Handle("/hello/{name}", PathVariableHandler).Methods("GET", "PUT")
+	err := http.ListenAndServe(CONN_PORT, router)
+	if err != nil {
+		log.Fatal("error starting http server : ", err)
+		return
+	}
+}
--- TCP_Server/tcp_server.go
+++ TCP_Server/tcp_server.go
@@ -1,1 +1,43 @@
-package TCP_Server
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"net"
+)
+
+const (
+	CONN_PORT = ":8080"
+	CONN_TYPE = "tcp"
+)
+
+func handleRequest(conn net.Conn) {
+	message, err := bufio.NewReader(conn).ReadString('\n')
+	if err != nil {
+		fmt.Println("Error reading:", err.Error())
+	}
+	fmt.Print("Message Received from the client: ", string(message))
+	conn.Write([]byte(message + "\n"))
+	conn.Close()
+}
+
+func main() {
+	listener, err := net.Listen(CONN_TYPE, CONN_PORT)
+
+	if err != nil {
+		log.Fatal("Error starting tcp server: ", err)
+	}
+
+	defer listener.Close()
+
+	log.Println("Listening on: " + CONN_PORT)
+
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			log.Fatal("Error accepting: ", err.Error())
+		}
+		go handleRequest(conn)
+	}
+}
